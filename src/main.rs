9a0c463809bfc57148e52f567e30c71f2d586df5
x2flip <gianni.m.baudino@gmail.com>
2023-09-02 14:13:10 -0700

    Added basic block scheduling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use chrono::{DateTime, Local};
+use chrono::{DateTime, Duration, Local};
+use std::io::stdin;
 
+#[derive(Debug)]
 struct Resource {
     id: String,
     resource_type: ResourceType,
 }
+
+#[derive(Debug)]
 enum ResourceType {
     Person,
     Machine,
     Pallet,
 }
+
+#[derive(Debug)]
 struct Block {
     id: String,
     seq: u32,
@@ -21,24 +27,24 @@ struct Project {
     blocks: Vec<Block>,
 }
 fn main() {
-    let project1 = Project {
+    let mut project1 = Project {
         id: "project1".to_string(),
         description: "Project 1".to_string(),
         blocks: Vec::new(),
     };
-    let block1 = Block {
+    let mut block1 = Block {
         id: "QUOTE".to_string(),
         seq: 1,
         length: 5.5,
         resources: Vec::new(),
     };
-    let block2 = Block {
+    let mut block2 = Block {
         id: "REVIEW".to_string(),
         seq: 2,
         length: 5.5,
         resources: Vec::new(),
     };
-    let block3 = Block {
+    let mut block3 = Block {
         id: "BUILD".to_string(),
         seq: 3,
         length: 5.5,
@@ -46,6 +52,36 @@ fn main() {
     };
 
     let now: DateTime<Local> = Local::now();
+    project1.blocks.push(block1);
+    project1.blocks.push(block2);
+    project1.blocks.push(block3);
+    println!("Here is your project: {}", project1.description);
+    println!("Here are your steps: {:#?}", project1.blocks);
+    println!("Which step would you like to schedule?");
+    project1
+        .blocks
+        .iter()
+        .for_each(|block| println!("{}", block.id));
 
-    println!("Hello, world!");
+    println!(
+        "Enter a number corresponding sequence number between {} and {}",
+        project1.blocks.first().unwrap().seq,
+        project1.blocks.last().unwrap().seq
+    );
+    let mut block_id = String::new();
+    stdin().read_line(&mut block_id).unwrap();
+    println!("When do you want this operation to start?");
+
+    let mut scheduled_start = String::new();
+    stdin().read_line(&mut scheduled_start).unwrap();
+    let selected_block = project1
+        .blocks
+        .iter()
+        .find(|&x| x.seq == block_id.trim().parse::<u32>().unwrap())
+        .unwrap();
+    let duration = Duration::seconds((selected_block.length * 3600.0) as i64);
+    let mut end_time = now;
+    end_time += duration;
+    println!("Start Time: {}", now);
+    println!("End Time: {}", end_time);
 }
